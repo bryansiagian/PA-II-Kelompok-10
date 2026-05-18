a35ec657dec09694c808c28e36430059412b3314
gresiadelia <deliagresia44@gmail.com>
2026-05-18 08:53:04 +0700

    Update Warehouse.php

--- distribusi_obat/app/Models/Warehouse.php
+++ distribusi_obat/app/Models/Warehouse.php
@@ -7,6 +7,6 @@ use App\Traits\Blameable;
 
 class Warehouse extends Model {
     use Blameable;
-    protected $fillable = ['code', 'name', 'location', 'active'];
+    protected $fillable = ['code', 'name', 'location', 'active', 'created_by','updated_by','created_at','updated_at'];
     public function products() { return $this->hasMany(Product::class); }
 }
